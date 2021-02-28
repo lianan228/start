449c2f2138feb9cb7f20cf4a7be9941a719d6bce
lianan228 <2273663522@qq.com>
2021-02-28 13:09:44 +0800

    second commit

--- Project_Date_Save/浮点数在内存中的存储.c
+++ Project_Date_Save/浮点数在内存中的存储.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
-int main()
-{
-	double d = 1E10;
-	printf("%lf\n", d);//1 X 10的十次方
-	return 0;
-}
+//int main()
+//{
+//	double d = 1E10;
+//	printf("%lf\n", d);//1 X 10的十次方
+//	return 0;
+//}
+
+
+//浮点数存储
+
+//9.0
+//1001.0
+//(-1)^0 * 1.001 * 2^3
+//(-1)^s * M     * 2^E
+//s - 0
+//M - 1.001
+//E - 3
