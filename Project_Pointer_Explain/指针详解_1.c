be8417aae01f788c87a868ce95f4e6d0b933bb40
lianan228 <2273663522@qq.com>
2021-03-02 23:33:31 +0800

    third commit

--- Project_Pointer_Explain/指针详解_1.c
+++ Project_Pointer_Explain/指针详解_1.c
@@ -105,14 +105,14 @@ int main()
 	int arr2[] = { 2,3,4,5,6 };
 	int arr3[] = { 3,4,5,6,7 };
 	
-	int parr[] = { arr1,arr2,arr3 };
+	int* parr[] = { arr1,arr2,arr3 };
 	int i;
 	for (i = 0; i < 3; i++)
 	{
 		int j;
 		for (j = 0; j < 5; j++)
 		{
-			printf("%d ", (parr[i] + j));
+			printf("%d ", *(parr[i] + j));
 		}
 		printf("\n");
 	}
