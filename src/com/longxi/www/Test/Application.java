cbca397042aaee48b9f6fc3aa7891eed22cd9a0a
lianan228 <2273663522@qq.com>
2021-04-27 20:43:24 +0800

    first commit

--- src/com/longxi/www/Test/Application.java
+++ src/com/longxi/www/Test/Application.java
@@ -2,18 +2,12 @@ package com.longxi.www.Test;
 
 public class Application {
     public static void main(String[] args) {
-        Demo demo = new Demo01();
-
-        //demo.setB(100);
-        //System.out.println(demo.getB());//100
-        System.out.println(demo.getB());//40
-        demo.run();//父类方法被子类重写 变为 子类run
-
-
-        //公有属性可直接被调用
-        demo.a = 1;
-        System.out.println(demo.a);//1
-
-
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            if(i%7!=0){
+                sum+=i;
+            }
+        }
+        System.out.println(sum);
     }
 }
